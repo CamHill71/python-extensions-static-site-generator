0c304cfb7a9bf0bd3fca594459d4a144484fef8f
Cameron Hill <87802054+NMTChill@users.noreply.github.com>
2022-12-12 12:11:12 +0800

    a

--- ssg/hooks.py
+++ ssg/hooks.py
@@ -16,6 +16,7 @@ def register(hook,order=0):
 
     def register_callback(func):
         """ """
+        _callbacks.setdefault(hook,{}).setdefault(order,[]).append(func)
         return func
 
     return register_callback
