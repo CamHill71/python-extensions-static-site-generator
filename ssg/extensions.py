70f6b6499daf553964f037ea612786a22d8a9801
Cameron Hill <87802054+NMTChill@users.noreply.github.com>
2022-12-12 11:32:33 +0800

    s

---
+++ ssg/extensions.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec 12 11:26:38 2022
+
+@author: Cameron Hill
+"""
+
+
+import sys
+
+import importlib
+from pathlib import Path
+
+
+def load_module(directory,name):
+    """ """
+    sys.path.insert(directory)
+
