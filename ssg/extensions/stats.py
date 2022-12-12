5631e631dfcdfb699edba4649b75773d72ccd4d3
Cameron Hill <87802054+NMTChill@users.noreply.github.com>
2022-12-12 13:16:52 +0800

    s

---
+++ ssg/extensions/stats.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec 12 11:26:38 2022
+
+@author: Cameron Hill
+"""
+
+
+
+from ssg import hooks
+import time
+
+
+start_time = None
+total_written = 0
+
+
+@hooks.register("start_build")
+def start_build():
+    """ """
+    global start_time
+    start_time = time.localtime()
