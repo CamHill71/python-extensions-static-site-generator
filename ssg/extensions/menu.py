9a9c0d07b5f9ee6f4cc4b226acf0c65bd6911285
Cameron Hill <87802054+NMTChill@users.noreply.github.com>
2022-12-12 12:31:22 +0800

    s

--- ssg/extensions/menu.py
+++ ssg/extensions/menu.py
@@ -11,9 +11,8 @@ from ssg import hooks,parsers
 
 files = []
 
-
+@hooks.register("collect_files")
 def collect_files(source,site_parsers):
-    """  """
-    hooks.register("collect_files")
+    """  """   
 
-    valid = lambda p:isinstance(p)
+    valid = lambda p: not isinstance(p,parsers.ResourceParser)
