40c24a753960a0864d36e4b64f413a6a8529c87a
Cameron Hill <87802054+NMTChill@users.noreply.github.com>
2022-12-12 12:17:18 +0800

    x

--- ssg/hooks.py
+++ ssg/hooks.py
@@ -13,6 +13,7 @@ _callbacks = {}
 
 def register(hook,order=0):
     """ """
+    
 
     def register_callback(func):
         """ """
@@ -20,3 +21,10 @@ def register(hook,order=0):
         return func
 
     return register_callback
+
+def event(hook,*args):
+    """"""
+
+    for order in sorted(_callbacks.get(hook,{})):
+        for func in _callbacks[hook][order]:
+            func(*args)
